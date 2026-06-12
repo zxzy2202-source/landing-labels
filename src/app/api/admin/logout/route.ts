10bb9e60f94b4d1be9bd3e1b100aedab6a272120
aac089686-dot <aac089686@gmail.com>
2026-06-12 09:05:39 +0800

    Delete src directory

--- src/app/api/admin/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { cookies } from 'next/headers';
-import { NextResponse } from 'next/server';
-
-export async function POST() {
-  const cookieStore = await cookies();
-  cookieStore.delete('admin_token');
-  return NextResponse.json({ success: true });
-}
