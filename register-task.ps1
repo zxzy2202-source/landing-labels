eac6082dc3a9a985fc0b01d4add01b9b9ddf76f0
aac089686-dot <aac089686@gmail.com>
2026-06-12 09:08:25 +0800

    Delete register-task.ps1

--- register-task.ps1
+++
@@ -1,7 +0,0 @@
-$action = New-ScheduledTaskAction -Execute "cmd.exe" -Argument "/c D:\不干胶落地页\start-dev.bat"
-$trigger = New-ScheduledTaskTrigger -AtLogon
-$principal = New-ScheduledTaskPrincipal -UserId "$env:USERDOMAIN\$env:USERNAME" -LogonType Interactive -RunLevel Limited
-$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -MultipleInstances IgnoreNew
-
-Register-ScheduledTask -TaskName "ZhixinNextDev" -Action $action -Trigger $trigger -Principal $principal -Settings $settings -Force
-Write-Host "Task registered successfully"
